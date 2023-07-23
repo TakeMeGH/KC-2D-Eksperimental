e0772eb09cdc951ca1615b602677f32cc3ba50d9
Abdul Rafi <abdulrafi907@gmail.com>
2023-07-23 14:47:02 +0700

    add sliding

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,14 +10,17 @@ public class Movement : MonoBehaviour
     Vector2 baseSpeed = new Vector2(2, 0);
     [SerializeField] float jumpSpeed;
     [SerializeField] float moveSpeed;
+    Animator animator;
     bool isGrounded;
 
     // berapa lama spacebar di hold
     float curTime;
+    bool isSliding;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // loncat kecepatan 2
@@ -40,18 +43,30 @@ public class Movement : MonoBehaviour
         if(isGrounded == true && Input.GetKey(KeyCode.Space)){
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
         }
-
+        
+        if(isSliding){
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + 0.05f);
+        }
     }
 
-    private void OnCollisionStay2D(Collision2D other) {
+    private void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.tag == "Ground"){
             isGrounded = true;
+        }
+        if(other.gameObject.tag == "Sliding"){
+            isSliding = true;
+            animator.Play("SlidingAnimation");
         }
     }
 
     private void OnCollisionExit2D(Collision2D other) {
         if(other.gameObject.tag == "Ground"){
             isGrounded = false;
+        }
+        if(other.gameObject.tag == "Sliding"){
+            isSliding = false;
+            animator.Play("IdleAnimation");
+
         }
     }
 }      
